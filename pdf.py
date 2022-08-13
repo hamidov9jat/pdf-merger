663aa95906b72b73f685483df49bea69a05f8fe3
Nijat Hamidov <hamidov9jat@gmail.com>
2022-08-13 11:44:25 +0400

    mistakes corrected

--- pdf.py
+++ pdf.py
@@ -4,9 +4,10 @@ reader = PdfReader('dummy.pdf')
 meta = reader.metadata
 print(len(reader.pages))
 
-# All of the following could be None!
+# All the following could be None!
 print(f'{meta.author=}')
 print(f'{meta.creator=}')
 print(f'{meta.producer=}')
 print(f'{meta.subject=}')
 print(f'{meta.title=}')
+
