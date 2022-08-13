8ed0bab103e522cbd717334be48b960bd57a37eb
Nijat Hamidov <hamidov9jat@gmail.com>
2022-08-13 11:46:43 +0400

    commented out

--- pdf.py
+++ pdf.py
@@ -1,13 +1,16 @@
 from PyPDF2 import PdfReader
 
-reader = PdfReader('dummy.pdf')
-meta = reader.metadata
-print(len(reader.pages))
+# --------------------------------------------------
+# reader = PdfReader('dummy.pdf')
+# meta = reader.metadata
+# print(len(reader.pages))
+#
+# # All the following could be None!
+# print(f'{meta.author=}')
+# print(f'{meta.creator=}')
+# print(f'{meta.producer=}')
+# print(f'{meta.subject=}')
+# print(f'{meta.title=}')
+# --------------------------------------------------
 
-# All the following could be None!
-print(f'{meta.author=}')
-print(f'{meta.creator=}')
-print(f'{meta.producer=}')
-print(f'{meta.subject=}')
-print(f'{meta.title=}')
 
